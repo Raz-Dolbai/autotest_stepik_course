d6bad151ad089d506473ba1f6af01dc3268c200b
Raz_dolbai <napersson@mail.ru>
2022-02-10 20:54:06 +0300

    add comment

--- lesson_3/test_parser.py
+++ lesson_3/test_parser.py
@@ -6,3 +6,6 @@ link = "http://selenium1py.pythonanywhere.com/"
 def test_guest_should_see_login_link(browser):
     browser.get(link)
     browser.find_element(By.CSS_SELECTOR, "#login_link")
+
+# для запуска теста с терминале, находясь в папке с тестом
+#pytest -s -v --browser_name=firefox test_cmd.py
